c5806ee96dbbf9932ab5e370ecf4ea75ee91e961
dante_MAC <dantinr@gmail.com>
2020-11-17 21:54:42 +0800

    swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    name: 'zhangsan',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500
   },
 
 
