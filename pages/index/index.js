b7392e3b45628d818a4472fd1265954373d76798
dante <dantinr@gmail.com>
2020-11-19 08:26:44 +0800

    首页 上拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,9 +9,20 @@ Page({
     vertical: false,
     autoplay: true,
     interval: 2000,
-    duration: 500
+    duration: 500,
+    list:[],
+    page: 1,
+    pagesize:10,
   },
 
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log(111111)
+    this.data.page++;
+    this.getGoodsList();
+  },
 
 
   //点击登录
@@ -43,6 +54,28 @@ Page({
       }
     })
 
+  },
+
+  //获取商品数据
+  getGoodsList: function()
+  {
+    let _this = this;
+    wx.request({
+      url: 'http://shop.2004.com/api/goodslist',
+      data:{
+        page:_this.data.page,
+        size:_this.data.pagesize
+      },
+      header:{'content-type': 'application/json'},
+      success(res){
+        let new_list = _this.data.list.concat(res.data.data.list)
+
+        _this.setData({
+          //list: res.data.data.list
+          list: new_list
+        })
+      }
+    })
   },
 
   //获取storage
@@ -81,55 +114,7 @@ Page({
   },
 
   onLoad: function () {
-
-
-
-    let _this = this;
-    //获取首页商品列表
-    wx.request({
-      url: 'http://shop.2004.com/api/goodslist',
-      header:{'content-type': 'application/json'},
-      success(res){
-        _this.setData({
-          list: res.data.data.list
-        })
-      }
-    })
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    this.getGoodsList();
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
-      name2: 'zhangsan222'
-    })
-  }
+
 })
